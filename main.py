11f3b72f1db0195cb3444ccb322ba59c4df7ba97
Keizarov <y.keizarov@gmail.com>
2023-02-08 01:24:30 +0300

    Game: Rock, Paper, Scissors

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
 # Project: Rock, Paper, Scissors
-import pictures
-from pictures import ROCK, SCISSORS, PAPER
-
-print(pictures.ROCK)
-print(pictures.SCISSORS)
-print(pictures.PAPER)
+import pictures as pc
+import random
 
 
+def game():
+    computer_choice = random.randrange(1, 4)
+    user_choice = input("Your choice is:  ").lower()
+    if computer_choice == 1:
+        if user_choice == 'rock':
+            print(f"It's DRAW. User {pc.ROCK} vs computer {pc.ROCK}")
+            return 0
+        elif user_choice == "paper":
+            print(f"It's WIN. User {pc.PAPER} vs computer {pc.ROCK}")
+            return 1
+        else:
+            print(f"You are LOSE. User {pc.SCISSORS} vs computer {pc.ROCK}")
+            return 2
+    if computer_choice == 2:
+        if user_choice == 'rock':
+            print(f"You are LOSE. User {pc.ROCK} vs computer {pc.PAPER}")
+            return 2
+        elif user_choice == "paper":
+            print(f"It's DRAW. User {pc.PAPER} vs computer {pc.PAPER}")
+            return 0
+        else:
+            print(f"You WIN. User {pc.SCISSORS} vs computer {pc.PAPER}")
+            return 1
+    if computer_choice == 3:
+        if user_choice == 'rock':
+            print(f"You WIN. User {pc.ROCK} vs computer {pc.SCISSORS}")
+            return 1
+        elif user_choice == "paper":
+            print(f"You are LOSE. User {pc.PAPER} vs computer {pc.SCISSORS}")
+            return 2
+        else:
+            print(f"It's DRAW. User {pc.SCISSORS} vs computer {pc.SCISSORS}")
+            return 0
 
 
+game_status = True
+user_count = 0
+computer_count = 0
 
 
-
-
-
-
-
-# if __name__ == '__main__':
-#     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    print("Welcome to game.")
+    while game_status:
+        result = game()
+        if result == 1:
+            user_count += 1
+        elif result == 2:
+            computer_count += 1
+        elif result == 0:
+            user_count = user_count
+            computer_count = computer_count
+        control_question = int(input(f"Now result: USER-{user_count} vs COMPUTER-{computer_count}. "
+                                 f"Do you play again? 1 - YES, 2 - NO. "))
+        if control_question == 2:
+            if user_count > computer_count:
+                print(f"Final result: USER-{user_count} vs COMPUTER-{computer_count}. "
+                      f"Congratulations, YOU WIN!")
+                game_status = False
+            elif user_count < computer_count:
+                print(f"Final result: USER-{user_count} vs COMPUTER-{computer_count}. "
+                      f"YOU LOSE...")
+                game_status = False
+            elif user_count == computer_count:
+                print(f"Final result: USER-{user_count} vs COMPUTER-{computer_count}. "
+                      f"Congratulations, IT'S DRAW!")
+                game_status = False
